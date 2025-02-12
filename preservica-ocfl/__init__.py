7c3f7cfd22881d601fb9ab5cc4a59afde1734868
James Carr <carj@users.noreply.github.com>
2025-02-12 13:46:45 +0000

    Update __init__.py

--- preservica-ocfl/__init__.py
+++ preservica-ocfl/__init__.py
@@ -1,1 +1,17 @@
+"""
+preserva-ocfl  module definition
 
+A python module for creating a local OCFL storage root from a Preservica repository
+
+author:     James Carr
+licence:    Apache License 2.0
+
+"""
+
+
+__author__ = "James Carr (drjamescarr@gmail.com)"
+
+# Version of the preserva-tweet package
+__version__ = "0.0.1"
+
+__license__ = "Apache License Version 2.0"
