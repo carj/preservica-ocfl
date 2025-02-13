ab09618619220eb069572be1e31d74ef77cf7105
carj <drjamescarr@gmail.com>
2025-02-13 12:46:44 +0000

    add support for multiple export threads

--- preservica-ocfl/__main__.py
+++ preservica-ocfl/__main__.py
@@ -10,22 +10,45 @@ licence:    Apache License 2.0
 import argparse
 import os.path
 import pathlib
+import queue
 import sys
 import uuid
+import logging
 import zipfile
+from concurrent.futures import ThreadPoolExecutor, Future
 from io import BytesIO
+
 from ocflcore import *
 from datetime import datetime, timezone
 
 from ocflcore.persistence.transaction import Transaction
 from pyPreservica import *
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
 SPEC_FILE: str = "https://ocfl.io/1.1/spec/"
 ROOT_NAMASTE: str = "0=ocfl_1.1"
 OBJECT_NAMASTE: str = "0=ocfl_object_1.1"
 ROOT_LAYOUT: str = "ocfl_layout.json"
 
+
+class ThreadPoolExecutorWithQueueSizeLimit(ThreadPoolExecutor):
+    """
+        Extend the ThreadPoolExecutor to add a work queue size limit
+        This stops the queue from growing too large and causing memory issues
+
+    """
+    def __init__(self, maxsize=50, *args, **kwargs):
+        super(ThreadPoolExecutorWithQueueSizeLimit, self).__init__(*args, **kwargs)
+        self._work_queue = queue.Queue(maxsize=maxsize)
+
+
 class PreservicaRepository(OCFLRepository):
+    """
+        Extend the OCFLRepository to add a method to check if an object exists in the storage root
+    """
 
     def exists(self, obj_id):
         o = OCFLObject(obj_id)
@@ -41,7 +64,11 @@ class PreservicaRepository(OCFLRepository):
 
 
 class PreservicaVersion(OCFLVersion):
+    """
+        Extend the OCFLVersion to add the user and system metadata
 
+        Use the Preservica username for the OCFL user address
+    """
     def __init__(self, creation_time, username: str, system: str, message: str = "Initial Export"):
         """Constructor."""
         super().__init__(creation_time)
@@ -52,9 +79,12 @@ class PreservicaVersion(OCFLVersion):
 class TruncatedNTripleUuid(StorageLayout):
     """
     Truncated n-tuple Tree use 2 hex digits for each level of the first (32 bits) part of the uuid
-    """
 
-    description = "Object structure is truncated n-tuple Tree which use 2 hex digits for each level of the first (32 bits) part of the Preservica uuid"
+    """
+    description = """
+        Object structure is truncated n-tuple Tree which use 2 hex digits for each 
+        level of the first (32 bits) part of the Preservica uuid
+        """
     extension = ""
 
     def path_for(self, obj):
@@ -68,49 +98,81 @@ class TruncatedNTripleUuid(StorageLayout):
         return object_path
 
 
-def populate(repository: PreservicaRepository, folder: Folder, entity: EntityAPI, search: ContentAPI):
+def export_opex(entity: EntityAPI, reference: str, repository: PreservicaRepository) -> str:
+    """
+    Export the asset as an OPEX package
+
+    :param entity: The Preservica EntityAPI object
+    :param reference: The Preservica Asset object reference
+    :param repository: The OCFL Repository object
+
+    :return: The stored OCFL object id
+    """
+    # Export the Asset block waiting for the opex package to be downloaded.
+    asset: Asset = entity.asset(reference)
+    opex_package = entity.export_opex_sync(asset, IncludeContent="Content", IncludeMetadata="Metadata",
+                                           IncludedGenerations="All", IncludeParentHierarchy="false")
+
+    # Create the OCFL object
+    ocfl_version = PreservicaVersion(datetime.now(timezone.utc), entity.username, f"{entity.server} ({entity.tenant})")
+
+    ocfl_object = OCFLObject(reference)
+
+    # Unzip the OPEX package into the correct folders
+    with zipfile.ZipFile(opex_package, "r") as zip_ref:
+        for f in zip_ref.filelist:
+            if f.filename.endswith(".pax.zip.opex"):
+                ocfl_file: StreamDigest = StreamDigest(BytesIO(zip_ref.read(f)))
+                ocfl_version.files.add(f.filename, ocfl_file.stream, ocfl_file.digest)
+            elif f.filename.endswith("pax.zip"):
+                zfiledata = BytesIO(zip_ref.read(f.filename))
+                with zipfile.ZipFile(zfiledata, "r") as zip_pax_ref:
+                    for ff in zip_pax_ref.filelist:
+                        if ff.is_dir() is False:
+                            ocfl_file: StreamDigest = StreamDigest(BytesIO(zip_pax_ref.read(ff)))
+                            ocfl_version.files.add(ff.filename, ocfl_file.stream, ocfl_file.digest)
+            else:
+                sys.exit("Unexpected file in OPEX package")
+
+    ocfl_object = OCFLObject(reference)
+    ocfl_object.versions.append(ocfl_version)
+    repository.add(ocfl_object)
+    os.remove(opex_package)
+
+    return ocfl_object.id
+
+def object_added(future: Future):
+    obj_id = future.result()
+    logger.info(f"Object {obj_id} added to OCFL storage root")
+
+def populate(repository: PreservicaRepository, folder: Folder, entity: EntityAPI, search: ContentAPI, num_threads: int):
 
     filter_values = {"xip.document_type": "IO"}
     if folder is not None:
         filter_values["xip.parent_hierarchy"] = folder.reference
-    search.callback = search.ReportProgressCallBack()
-    for hit in search.search_index_filter_list(query="%", filter_values=filter_values):
-        reference: str = hit['xip.reference']
 
-        # Does the object exist in the OCFL storage root
-        if repository.exists(reference):
-            print(f"Object {reference} already exists in the OCFL storage root")
-            continue
+    num_hits: int = search.search_index_filter_hits(query="%", filter_values=filter_values)
 
-        # Export the Asset
-        asset: Asset = entity.asset(reference)
-        opex_package = entity.export_opex_sync(asset, IncludeContent="Content", IncludeMetadata="Metadata",
-                                               IncludedGenerations="All", IncludeParentHierarchy="false")
+    print(f"Found {num_hits} objects to export")
 
-        # Create the OCFL object
-        ocfl_version = PreservicaVersion(datetime.now(timezone.utc), entity.username, f"{entity.server} ({entity.tenant})")
+    count: int = 0
 
-        ocfl_object = OCFLObject(reference)
+    with ThreadPoolExecutorWithQueueSizeLimit(max_workers=num_threads, maxsize=num_threads*2) as executor:
 
-        # Unzip the OPEX package into the correct folders
-        with zipfile.ZipFile(opex_package, "r") as zip_ref:
-            for f in zip_ref.filelist:
-                if f.filename.endswith(".pax.zip.opex"):
-                    ocfl_file: StreamDigest = StreamDigest(BytesIO(zip_ref.read(f)))
-                    ocfl_version.files.add(f.filename, ocfl_file.stream, ocfl_file.digest)
-                elif f.filename.endswith("pax.zip"):
-                    zfiledata = BytesIO(zip_ref.read(f.filename))
-                    with zipfile.ZipFile(zfiledata, "r") as zip_pax_ref:
-                        for ff in zip_pax_ref.filelist:
-                            if ff.is_dir() is False:
-                                ocfl_file: StreamDigest = StreamDigest(BytesIO(zip_pax_ref.read(ff)))
-                                ocfl_version.files.add(ff.filename, ocfl_file.stream, ocfl_file.digest)
-                else:
-                    sys.exit("Unexpected file in OPEX package")
+        for hit in search.search_index_filter_list(query="%", filter_values=filter_values):
 
-        ocfl_object.versions.append(ocfl_version)
-        repository.add(ocfl_object)
-        os.remove(opex_package)
+            count += 1
+            reference: str = hit['xip.reference']
+
+            # Does the object exist in the OCFL storage root
+            if repository.exists(reference):
+                logger.info(f"Object {reference} already exists in the OCFL storage root. Skipping...")
+                continue
+
+            future: Future = executor.submit(export_opex, entity, reference, repository)
+            future.add_done_callback(object_added)
+
+            logger.info(f"Processing item {count} of {num_hits}:  Asset {reference}")
 
 
 def init(args):
@@ -127,8 +189,17 @@ def init(args):
     password = cmd_line['password']
     server = cmd_line['server']
 
+
+    num_threads = cmd_line['threads']
+
+    if num_threads < 1:
+        num_threads = 1
+    if num_threads > 8:
+        num_threads = 8
+
+
     if (username is not None) and (password is not None) and (server is not None):
-        print(f"Using credentials from command line")
+        logger.info(f"Using credentials from command line")
         entity: EntityAPI = EntityAPI(username=username, password=password, server=server)
         search: ContentAPI = ContentAPI(username=username, password=password, server=server)
     else:
@@ -138,14 +209,14 @@ def init(args):
     folder: Folder = None
     if collection is not None:
         folder = entity.folder(collection)
-        print(f"Populating OCFL storage root with objects from {folder.title}")
+        logger.info(f"Populating OCFL storage root with objects from {folder.title}")
     else:
-        print(f"Populating OCFL storage root with objects from all collections")
+        logger.info(f"Populating OCFL storage root with objects from all collections")
 
     root = StorageRoot(TruncatedNTripleUuid())
 
     storage_root = cmd_line['storage_root']
-    print(f"Creating OCFL storage root at {storage_root}")
+    logger.info(f"Creating OCFL storage root at {storage_root}")
 
     storage = FileSystemStorage(storage_root)
     workspace_storage = FileSystemStorage(f"{storage_root}_WRKSP")
@@ -158,7 +229,7 @@ def init(args):
                 f.write(r.text)
 
 
-    populate(repository, folder, entity, search)
+    populate(repository, folder, entity, search, num_threads)
 
 
 def main():
@@ -174,9 +245,13 @@ def main():
         description='Create a local OCFL storage root from a Preservica repository',
         epilog='')
 
-    cmd_parser.add_argument("-r", "--storage-root", type=pathlib.Path, help="The OCFL Storage Root", required=True)
+    cmd_parser.add_argument("-r", "--storage-root", type=pathlib.Path, help="The OCFL Storage Root",
+                            required=True)
     cmd_parser.add_argument("-c", "--collection", type=str, help="The Preservica parent collection uuid",
                             required=False)
+
+    cmd_parser.add_argument("-t", "--threads", type=int, help="The number of export threads, defaults to 1",
+                            required=False, default=1)
 
     cmd_parser.add_argument("-u", "--username", type=str,
                             help="Your Preservica username if not using credentials.properties", required=False)
